ee680b0dbac9775600ac9e0c7acde8d75414d813
Khang Hoang Tran <tranhoangkhang09112001@gmail.com>
2025-09-02 08:59:23 +0700

    new simple deploy

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  subscription_id=var.subscription_id
+  client_secret=var.client_secret
+  tenant_id=var.tenant_id
+  client_id=var.client_id
+  features {}
+}
+
+
+
+module "vnet" {
+  source  = "app.terraform.io/example-org-15045d/vnet/azurerm"
+  version = "1.0.0"
+
+  enabled             = true
+  name                = "test"
+  location            = "Korea South"
+  resource_group_name = "sample-labs"
+  address_space       = ["10.0.0.0/16"]
+  
+  subnets = [
+    {
+      name             = "subnet1"
+      address_prefixes = ["10.0.1.0/24"]
+    },
+    {
+      name             = "subnet2"
+      address_prefixes = ["10.0.2.0/24"]
+    }
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+    description = "The subscription ID for the Azure account"
+    type        = string
+}
+
+variable "client_secret" {
+    description = "The subscription ID for the Azure account"
+    type        = string
+}
+
+variable "tenant_id" {
+    description = "The subscription ID for the Azure account"
+    type        = string
+}
+
+variable "client_id" {
+    description = "The subscription ID for the Azure account"
+    type        = string
+}
